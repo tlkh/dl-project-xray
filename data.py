f0d2878b94e9cf8196be86887cd6b68927300c60
Timothy Liu <tlkh.xms@gmail.com>
2020-04-11 04:42:33 +0000

    Added cleaned version of reports csv

--- data.py
+++ data.py
@@ -66,10 +66,8 @@ def collate_fn(data):
     # Sort a data list by caption length (descending order).
     data.sort(key=lambda x: len(x[1]), reverse=True)
     images, captions = zip(*data)
-
     # Merge images (from tuple of 3D tensor to 4D tensor).
     images = torch.stack(images, 0)
-
     # Merge captions (from tuple of 1D tensor to 2D tensor).
     lengths = [len(cap) for cap in captions]
     targets = torch.zeros(len(captions), max(lengths)).long()
