660ea1c3510a30646b1909a881d60997aa2aa9fd
Emrys-Hong <hongpengfei.emrys@gmail.com>
2020-04-25 18:02:55 +0800

    Added blue evaluation

--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import numpy as np
 import math
 from config import config
+import os
+import re
+import subprocess
+import tempfile
 
 def gen_embeddings(vocab):
     """
@@ -42,3 +46,76 @@ def share_embedding(vocab, pretrain=True):
         embedding.lut.weight.data.copy_(torch.FloatTensor(pre_embedding))
         embedding.lut.weight.data.requires_grad = True
     return embedding
+
+"""BLEU metric implementation.
+"""
+def moses_multi_bleu(hypotheses, references, lowercase=False):
+    """Calculate the bleu score for hypotheses and references
+    using the MOSES ulti-bleu.perl script.
+    Args:
+    hypotheses: A numpy array of strings where each string is a single example.
+    references: A numpy array of strings where each string is a single example.
+    lowercase: If true, pass the "-lc" flag to the multi-bleu script
+    Returns:
+    The BLEU score as a float32 value.
+    """
+
+    if np.size(hypotheses) == 0:
+        return np.float32(0.0)
+
+    multi_bleu_path = "multi-bleu.perl"
+    os.chmod(multi_bleu_path, 0o755)
+
+
+    # Dump hypotheses and references to tempfiles
+    hypothesis_file = tempfile.NamedTemporaryFile()
+    hypothesis_file.write("\n".join(hypotheses).encode("utf-8"))
+    hypothesis_file.write(b"\n")
+    hypothesis_file.flush()
+    reference_file = tempfile.NamedTemporaryFile()
+    reference_file.write("\n".join(references).encode("utf-8"))
+    reference_file.write(b"\n")
+    reference_file.flush()
+
+
+     # Calculate BLEU using multi-bleu script
+    with open(hypothesis_file.name, "r") as read_pred:
+        bleu_cmd = [multi_bleu_path]
+        if lowercase:
+            bleu_cmd += ["-lc"]
+        bleu_cmd += [reference_file.name]
+        try:
+            bleu_out = subprocess.check_output(bleu_cmd, stdin=read_pred, stderr=subprocess.STDOUT)
+            bleu_out = bleu_out.decode("utf-8")
+            bleu_score = re.search(r"BLEU = (.+?),", bleu_out).group(1)
+            bleu_score = float(bleu_score)
+        except subprocess.CalledProcessError as error:
+            if error.output is not None:
+                print("multi-bleu.perl script returned non-zero exit code")
+                print(error.output)
+                bleu_score = np.float32(0.0)
+
+    # Close temp files
+    hypothesis_file.close()
+    reference_file.close()
+    return bleu_score
+
+class BeamSearchNode(object):
+    def __init__(self, hiddenstate, previousNode, wordId, logProb, length):
+        '''
+        :param hiddenstate:
+        :param previousNode:
+        :param wordId:
+        :param logProb:
+        :param length:
+        '''
+        self.h = hiddenstate
+        self.prevNode = previousNode
+        self.wordid = wordId
+        self.logp = logProb
+        self.leng = length
+
+    def eval(self, alpha=1.0):
+        reward = 0
+        # Add here a function for shaping a reward
+        return self.logp / float(self.leng - 1 + 1e-6) + alpha * reward
