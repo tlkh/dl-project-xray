182aa1eab029fed22cb8b15bbd8c19839824272e
Timothy Liu <tlkh.xms@gmail.com>
2020-04-25 17:22:17 +0000

    Update attention model

--- models_attn.py
+++ models_attn.py
@@ -9,7 +9,7 @@ class EncoderCNN(torch.nn.Module):
     def __init__(self, embed_size, num_classes):
         """Load the pretrained ResNet and replace top fc layer."""
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet18(pretrained=True)
+        resnet = models.inception_v3(pretrained=True)
         modules = list(resnet.children())[:-2]
         self.resnet = torch.nn.Sequential(*modules)
         self.dropout = torch.nn.Dropout(0.1)
